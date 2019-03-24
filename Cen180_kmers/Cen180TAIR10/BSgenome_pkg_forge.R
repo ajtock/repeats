88fa2c157581cc7c715904d3eaaaab4fcda6420f
ajtock <andytock@gmail.com>
2019-03-24 20:34:46 +0000

    Cen180TAIR10

---
+++ Cen180_kmers/Cen180TAIR10/BSgenome_pkg_forge.R
@@ -0,0 +1,23 @@
+#!/applications/R/R-3.5.0/bin/Rscript
+
+# Forge a BSgenome data package for concatenated Cen180 sequences in
+# nanopore contigs (concatenated by Ian Henderson)
+
+# Usage:
+# ./BSgenome_pkg_forge.R Cen180TAIR10
+
+args <- commandArgs(trailingOnly = T)
+packageNamePt4 <- args[1]
+
+library(BSgenome)
+
+seed_files <- system.file("extdata", "HendersonLab", package = "BSgenome") 
+tail(list.files(seed_files, pattern = "-seed$"))
+
+Cen180cat_seed <- list.files(seed_files,
+                             pattern = paste0("\\.", packageNamePt4, "-seed$"),
+                             full.names = TRUE)
+cat(readLines(Cen180cat_seed), sep = "\n")
+
+forgeBSgenomeDataPkg(Cen180cat_seed)
+
